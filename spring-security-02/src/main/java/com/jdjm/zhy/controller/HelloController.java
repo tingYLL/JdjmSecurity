b2989e66a56e4277a641959f27b031738e6907e8
ZhengHongyi <111111@163.com>
2023-12-27 22:52:09 +0800

    多线程情况下获取用户数据SecurityContextHolder

--- spring-security-02/src/main/java/com/jdjm/zhy/controller/HelloController.java
+++ spring-security-02/src/main/java/com/jdjm/zhy/controller/HelloController.java
@@ -12,6 +12,21 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello() {
+//        Authentication authentication = SecurityContextHolder
+//                .getContext().getAuthentication();
+//        User principal = (User) authentication.getPrincipal();
+//        System.out.println("身份 :"+principal.getUsername());
+//        System.out.println("凭证 :"+authentication.getCredentials());
+//        System.out.println("权限 :"+authentication.getAuthorities());
+
+        new Thread(()->{
+            Authentication authentication = SecurityContextHolder
+                    .getContext().getAuthentication();
+            User principal = (User) authentication.getPrincipal();
+            System.out.println("身份 :"+principal.getUsername());
+            System.out.println("凭证 :"+authentication.getCredentials());
+            System.out.println("权限 :"+authentication.getAuthorities());
+        }).start();
         return "hello security";
     }
 
